b06402fe9cf5ad2e25725bc004294832f0c66e13
Iosif <dev@iosifv.com>
2024-11-07 17:41:59 +0000

    [main] - added a chart

--- src/app/_components/Chart.tsx
+++ src/app/_components/Chart.tsx
@@ -14,15 +14,15 @@ interface ChartProps {
 
 export default function Chart({ data, config }: ChartProps) {
   return (
-    // <ChartContainer config={config} className="min-h-[200px] w-full">
-    //   <ResponsiveContainer width="100%" height={300}>
-    //     <BarChart data={data}>
-    //       <XAxis dataKey="month" />
-    //       <YAxis />
-    //       <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-    //       <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-    //     </BarChart>
-    //   </ResponsiveContainer>
-    // </ChartContainer>
+    <ChartContainer config={config} className="min-h-[200px] w-full">
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        </BarChart>
+      </ResponsiveContainer>
+    </ChartContainer>
   )
 }
